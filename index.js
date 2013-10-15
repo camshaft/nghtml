257b09e8de43203ba4f4dafc575e80b1c6ac54fb
Cameron Bytheway <cameron@theflokk.com>
2013-10-15 16:30:29 -0600

    refactor

--- index.js
+++ index.js
@@ -1,33 +1,42 @@
 /**
  * Module dependencies
  */
-var fs = require("fs")
-  , path = require("path")
-  , read = fs.readFileSync;
 
-var template = read(path.join(__dirname,"template.js"), 'utf8');
+var fs = require('fs');
+var path = require('path');
+var read = fs.readFileSync;
+
+/**
+ * Load the angular cache template
+ */
+
+var template = read(path.join(__dirname, 'template.js'), 'utf8');
 
 /**
  * Expose plugin
+ *
+ * @param {Object} options
  */
+
 module.exports = function(options) {
   if (!options) options = {};
-  options.extension = options.extension || ".nghtml";
-  options.module = options.module || "partials";
-  options.webroot = options.webroot || "";
+  options.extension = options.extension || '.nghtml';
+  options.module = options.module || 'partials';
+  options.webroot = options.webroot || '';
   options.dev = typeof options.dev == "undefined" ? false : options.dev;
+  options.confProp = options.confProp || 'templates';
 
-  var nextLineEscape = options.dev ? "\"+\n\"" : " ";
+  var nextLineEscape = options.dev ? "\"+\n\"" : ' ';
 
   return function(builder) {
     builder.hook('before scripts', function(pkg){
 
-      var templates = (pkg.config||pkg.conf).templates;
+      var templates = (pkg.config || pkg.conf)[options.confProp];
       if (!templates) return;
 
       templates.forEach(function(file){
         var ext = path.extname(file);
-        if (options.extension != ext) return;
+        if (options.extension !== ext) return;
 
         var relativePath = path.relative(
           pkg.path(options.webroot),
@@ -44,7 +53,7 @@ module.exports = function(options) {
           options.module,
           nextLineEscape
         );
-        var newFile = path.dirname(file)+"/"+path.basename(file, options.extension) + '.js';
+        var newFile = path.dirname(file) + '/' + path.basename(file, options.extension) + '.js';
         pkg.addFile('scripts', newFile, js);
       });
     });
@@ -53,7 +62,7 @@ module.exports = function(options) {
 
 function compile(html, file, module, nextLineEscape) {
   return template
-    .replace(/\{\{path\}\}/g, "/"+file)
+    .replace(/\{\{path\}\}/g, '/' + file)
     .replace(/\{\{module\}\}/g, module)
     .replace(/\{\{content\}\}/g, html
       .replace(/\"/g, "\\\"")
